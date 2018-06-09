a3dec36aad983c8ce658b8c40fa8099f3fae9cc9
Mustafa Sertac Turkel <sertac.turkel@etstur.com>
2018-06-09 20:21:54 +0300

    rest-client implemented for otonom.py

--- datameer-rest-gateway/otonom.py
+++ datameer-rest-gateway/otonom.py
@@ -1,17 +1,26 @@
 import schedule
 import time
+import requests
+
+url = 'http://localhost:5000/test'
+
+
 class Otonom:
+
     def job(self):
-        print("I'm working...")
+        print "I'm working..."
+        response = requests.get(url)
+        print response.content
+
     def scheduler(self):
         schedule.every(1).minutes.do(self.job)
         schedule.every().hour.do(self.job)
-        schedule.every().day.at("16:11").do(self.job)
+        schedule.every().day.at("20:20").do(self.job)
         while 1:
             schedule.run_pending()
             time.sleep(1)
 
 
 if __name__ == '__main__':
-    o=Otonom()
+    o = Otonom()
     o.scheduler()
